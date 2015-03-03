0aa5f3c60e31215c926b274910b74cf6a7789621
Asad Saeeduddin <masaeedu@gmail.com>
2015-03-02 21:02:46 -0500

    Scaffolded angularjs app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uwasmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uwasmeApp
+ */
+angular.module('uwasmeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
